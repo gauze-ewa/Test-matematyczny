46c7c686c0c26b07bd0bd17509c141c19cad6ff3
Ewa Gauze <gauze.ewa@gmail.com>
2020-07-20 16:25:22 +0200

    dodanie1klas

---
+++ src/Calculate.java
@@ -0,0 +1,10 @@
+public class Calculate {    public static void main(String[] args) {
+    int a = 6;
+    double r = 3.6;
+    Calculator calculator = new Calculator();
+    calculator.isEven(a);
+    calculator.isOdd(a);
+    calculator.circleField(r);
+    calculator.power(a);
+}
+}
